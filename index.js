cddb30431cd0843efb5ef011f94c49829b8bfed8
github username <github email address>
2018-08-27 12:07:52 -0400

    instead of creating a function and defining it, you are creating a VARIABLE that equates to the definition of what that function will do and return.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+  return array.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(array) {
+  return array.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier =  function(x) {
+  return function (fare) {
+    return x * fare;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, x) {
+  return x(drivers);
+}
